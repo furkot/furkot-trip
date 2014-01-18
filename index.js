ef80d9276eb7f59496ca0e6befc07632f468ff6f
Natalia Kowalczyk <natalia.kowalczyk@gmail.com>
2014-01-17 19:43:02 -0500

    prepare url to add stop to trip as GET

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = furkotTrip;
 
+var destination = 'https://trips.furkot.com/trip';
 
 /*global window, document */
 
@@ -14,6 +15,38 @@ function furkotTrip() {
     return form.submit();
   }
 
+  function getUrl(data) {
+    if (data) {
+      if (Array.isArray(data)) {
+        data = data.map(function (stop, i) {
+          return toQuery(stop, 'stops[' + i + ']');
+        }).join('&');
+      }
+      else {
+        data = toQuery(data, 'stop');
+      }
+      if (data) {
+        data = '?' + data;
+      }
+    }
+    return destination + (data || '');
+  }
+
+  function toQuery(data, prefix) {
+    return Object.keys(data).map(toParam, {
+      data: data,
+      prefix: prefix
+    }).join('&');
+  }
+
+  function toParam(key) {
+    var data = this.data, prefix = this.prefix + '[' + key + ']';
+    if (typeof data[key] === 'object') {
+      return toQuery(data[key], prefix);
+    }
+    return [prefix, encodeURIComponent(data[key] || '')].join('=');
+  }
+
   function target() {
     var hidden = window.locationbar && !window.locationbar.visible;
     return hidden ? '_blank' : '_top';
@@ -22,7 +55,7 @@ function furkotTrip() {
   function createForm() {
     var form = document.createElement('form');
     form.style.display = 'none';
-    form.action = 'https://trips.furkot.com/trip';
+    form.action = destination;
     form.method = 'post';
     form.target = target();
     stops = document.createElement('input');
@@ -34,6 +67,7 @@ function furkotTrip() {
   }
 
   return {
-    plan: plan
+    plan: plan,
+    getUrl: getUrl
   };
 }
